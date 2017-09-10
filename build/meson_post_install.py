55299bb0ef3fb013bf5e8af73ff880ecab7f103b
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-09-10 11:05:54 +0200

    add missing post install

---
+++ build/meson_post_install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from os import environ, path
+from subprocess import call
+
+destdir = environ.get('DESTDIR', '')
+
+if not destdir:
+    prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+    icons_dir = path.join(prefix, 'share/icons/hicolor')
+    schema_dir = path.join(prefix, 'share/glib-2.0/schemas')
+
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', icons_dir])
+    print("Installing new Schemas")
+    call(['glib-compile-schemas', schema_dir])
