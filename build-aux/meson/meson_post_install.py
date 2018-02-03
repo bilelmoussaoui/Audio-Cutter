ffd3aecafdea28b3646697344cc7316deb8578b2
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2018-02-03 14:06:51 +0100

    clean the build system

--- build-aux/meson/meson_post_install.py
+++ build-aux/meson/meson_post_install.py
@@ -9,8 +9,11 @@ if not destdir:
     prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
     icons_dir = path.join(prefix, 'share/icons/hicolor')
     schema_dir = path.join(prefix, 'share/glib-2.0/schemas')
+    desktop_dir = path.join(prefix, 'share/applications')
 
     print('Updating icon cache...')
     call(['gtk-update-icon-cache', '-qtf', icons_dir])
     print("Installing new Schemas")
     call(['glib-compile-schemas', schema_dir])
+    print("Updating desktop database")
+    call(["update-desktop-database", desktop_dir])
